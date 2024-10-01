3d198d95cf6e627727e528997ad8219d382f94f8
Divya123solanki <solankidivya817@gmail.com>
2024-10-01 18:39:53 +0530

    Add files via upload

---
+++ CubeEdge19.java
@@ -0,0 +1,9 @@
+public class CubeEdge19 {
+    public static void main(String[] args) {
+        int c=7,l=7,b=4,h=8,v,v1,result;
+        v=c*c*c;
+        v1=l*b*h;
+        result=v-v1;
+        System.out.println(result);
+    }
+}
